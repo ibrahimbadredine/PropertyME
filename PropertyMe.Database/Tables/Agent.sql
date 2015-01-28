875484452207d42e0b964205482adde72bf992fd
ibrahimbadredine <ibrahim_badredine@yahoo.com>
2015-01-28 08:28:45 +0200

    DB changes and UI updated

--- PropertyMe.Database/Tables/Agent.sql
+++ PropertyMe.Database/Tables/Agent.sql
@@ -2,11 +2,8 @@
 (
 	[ID] INT NOT NULL PRIMARY KEY, 
     [UserID] BIGINT NOT NULL, 
-    [Mobile] NVARCHAR(20) NOT NULL, 
-    [PictureID] BIGINT NOT NULL, 
     [AgencyID] INT NOT NULL, 
-    [Description] NVARCHAR(300) NULL, 
+    [AboutMe] NVARCHAR(300) NULL, 
     CONSTRAINT [FK_Agent_User] FOREIGN KEY ([UserID]) REFERENCES [User]([ID]), 
-    CONSTRAINT [FK_Agent_Picture] FOREIGN KEY ([PictureID]) REFERENCES [Picture]([ID]),
-	CONSTRAINT [FK_Agent_Agency] FOREIGN KEY ([AgencyID]) REFERENCES [Agency]([ID])
+    CONSTRAINT [FK_Agent_Agency] FOREIGN KEY ([AgencyID]) REFERENCES [Agency]([ID])
 )
--- PropertyMe.Database/Tables/User.sql
+++ PropertyMe.Database/Tables/User.sql
@@ -4,7 +4,9 @@
     [FirstName] NVARCHAR(20) NOT NULL, 
     [LastName] NVARCHAR(20) NOT NULL, 
     [Password] NVARCHAR(20) NOT NULL, 
-    [Email] NVARCHAR(50) NOT NULL, 
+	[Mobile] NVARCHAR(20) NOT NULL, 
+    [Email] NVARCHAR(50) NULL, 
+	[PictureID] bigint NULL,
     [RoleID] INT NOT NULL
 	CONSTRAINT [FK_User_Role] FOREIGN KEY ([RoleID]) REFERENCES [Role]([ID]),
 )
