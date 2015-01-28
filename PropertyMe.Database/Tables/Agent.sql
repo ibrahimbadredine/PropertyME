f8fec087d35d42ac5299f426ef69823610181c0d
ibrahimbadredine <ibrahim_badredine@yahoo.com>
2015-01-29 00:53:28 +0200

    Revert "Agent modify"
    This reverts commit d0fc4aab472d029259366e4922d634c7e24de604.

--- PropertyMe.Database/Tables/Agent.sql
+++ PropertyMe.Database/Tables/Agent.sql
@@ -4,7 +4,6 @@
     [UserID] BIGINT NOT NULL, 
     [AgencyID] INT NOT NULL, 
     [AboutMe] NVARCHAR(300) NULL, 
-    [isActive] BIT NOT NULL DEFAULT 0, 
     CONSTRAINT [FK_Agent_User] FOREIGN KEY ([UserID]) REFERENCES [User]([ID]), 
     CONSTRAINT [FK_Agent_Agency] FOREIGN KEY ([AgencyID]) REFERENCES [Agency]([ID])
 )
