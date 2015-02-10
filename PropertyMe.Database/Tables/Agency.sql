020eb0ab2fa08c0ba6ef652dee93f4e9f9fbaa29
ibrahimbadredine <ibrahim_badredine@yahoo.com>
2015-02-10 18:07:06 +0200

    updates

--- PropertyMe.Database/Tables/Agency.sql
+++ PropertyMe.Database/Tables/Agency.sql
@@ -1,11 +1,7 @@
 ﻿CREATE TABLE [dbo].[Agency]
 (
 	[ID] INT NOT NULL , 
-    [Name] NVARCHAR(50) NOT NULL, 
-	[Description] NVARCHAR(300) NOT NULL,
-    [PictureID] BIGINT NOT NULL, 
-    [Phone] NVARCHAR(20) NOT NULL, 
-    [Email] NVARCHAR(50) NOT NULL, 
+    [UserID] BIGINT NOT NULL, 
     CONSTRAINT [PK_Agency] PRIMARY KEY ([ID]), 
-    CONSTRAINT [FK_Agency_Picture] FOREIGN KEY ([PictureID]) REFERENCES [dbo].[Picture] ([ID]) 
+    CONSTRAINT [FK_Agency_User] FOREIGN KEY ([UserID]) REFERENCES [User]([ID]), 
 )
--- PropertyMe.Database/Tables/Agent.sql
+++ PropertyMe.Database/Tables/Agent.sql
@@ -3,8 +3,6 @@
 	[ID] INT NOT NULL PRIMARY KEY, 
     [UserID] BIGINT NOT NULL, 
     [AgencyID] INT NOT NULL, 
-    [AboutMe] NVARCHAR(300) NULL, 
-    [isActive] BIT NOT NULL DEFAULT 0, 
     CONSTRAINT [FK_Agent_User] FOREIGN KEY ([UserID]) REFERENCES [User]([ID]), 
     CONSTRAINT [FK_Agent_Agency] FOREIGN KEY ([AgencyID]) REFERENCES [Agency]([ID])
 )
