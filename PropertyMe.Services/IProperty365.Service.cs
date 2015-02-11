f06dad56d330e089626748e78db1c8dc755414e5
ibrahimbadredine <ibrahim_badredine@yahoo.com>
2015-02-11 18:36:48 +0200

    srv

---
+++ PropertyMe.Services/IProperty365.Service.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace PropertyMe.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IProperty365" in both code and config file together.
+    [ServiceContract]
+    public interface IProperty365Service
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "GetTest/")]
+        string GetTest();
+
+
+    }
+}
---
+++ PropertyMe.Services/Property365.Service.svc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace PropertyMe.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Property365" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Property365.svc or Property365.svc.cs at the Solution Explorer and start debugging.
+    public class Property365Service : IProperty365Service
+    {
+        public string GetTest()
+        {
+            return "testing service";
+        }
+    }
+}
