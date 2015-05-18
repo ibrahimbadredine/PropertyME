fdbe860cdac59287fad5a4d8bbd7fbca3be2a101
ibrahimbadredine <ibrahim_badredine@yahoo.com>
2015-05-18 23:33:35 +0300

    checking wcf api

---
+++ Property365.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Property365.WebApi.MvcApplication" Language="C#" %>
---
+++ Property365.WebApi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Property365.WebApi.Startup))]
+namespace Property365.WebApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
